4e95085e9580da3bdf2dc1e083076d4f60342151
Evren Esat Özkan <sleytr@gmail.com>
2016-10-16 15:19:13 +0300

    added some docstring added group related methods to AddressBook made Person.add behave like add (instead of overwrite)

--- abook.py
+++ abook.py
@@ -32,10 +32,15 @@ class Person:
         """
         handles assignment of multiple or singular properties
         """
-        for param in ['address', 'email', 'phone', 'group']:
-            if param in kwargs:
-                self.data["%s_set" % param].append(kwargs.pop(param))
-        self.data.update(kwargs)
+        for key, val in kwargs.items():
+            if key in ['name', 'last_name']:
+                self.data[key] = val
+            else:
+                key_set = '%s_set' % key
+                if key in self.data or key_set in self.data:
+                    if key_set in self.data:
+                        key, val = key_set, [val]
+                self.data[key] = list(set(self.data[key] + val))
 
 
 class AddressBook(DeBe):
@@ -50,9 +55,44 @@ class AddressBook(DeBe):
             self.groups[group].append(person.pk)
 
     def create_person(self, **kwargs):
+        """
+        Creates a Person object with given parameters,
+        adds it to address book.
+        Args:
+            name (str): Person's name
+            last_name (str): Person's last name
+            email (str): Person's email
+            address (str): Person's address
+            phone (str): Person's phone
+            group (str): Person's group
+            email_set (list): Update Person's email_set
+            address_set (list): Update Person's address_set
+            phone_set (list): Update Person's phone_set
+            group_set (list): Update Person's group_set
+        Returns:
+            Person: Just created Person object
+        """
         person = Person(**kwargs)
         self.add(person)
         return person
 
-    def group(self, name):
+    def add_group(self, name):
+        # explicit group creation is not required
         self.groups.setdefault(name, {})
+
+    def list_groups(self):
+        """
+        Returns:
+            list: List of group names
+        """
+        return self.groups.keys()
+
+    def get_group(self, name):
+        """
+        Args:
+            name (str): name of group
+
+        Returns:
+            list: Person list
+        """
+        return self._get_objects_by_ids(self.groups[name])
